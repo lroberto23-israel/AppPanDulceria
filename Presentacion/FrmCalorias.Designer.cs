8d77d14aec0a1aae558b5f029f009e6c257dea45
lroberto23-israel <e1400466676@uisrael.edu.ec>
2024-07-12 19:53:41 -0500

    Corrección Formulario

--- Presentacion/FrmCalorias.Designer.cs
+++ Presentacion/FrmCalorias.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdCaloria = new System.Windows.Forms.TextBox();
             this.lblIdCaloria = new System.Windows.Forms.Label();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnNuevo = new System.Windows.Forms.Button();
-            this.txtIdCaloria = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.dtgListaCalorias = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -45,7 +45,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtCantidad);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtIdCaloria);
             this.groupBox1.Controls.Add(this.lblIdCaloria);
@@ -54,6 +54,29 @@
             this.groupBox1.Size = new System.Drawing.Size(276, 153);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(111, 73);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(159, 20);
+            this.txtCantidad.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cantidad Calorias";
+            // 
+            // txtIdCaloria
+            // 
+            this.txtIdCaloria.Location = new System.Drawing.Point(111, 31);
+            this.txtIdCaloria.Name = "txtIdCaloria";
+            this.txtIdCaloria.Size = new System.Drawing.Size(159, 20);
+            this.txtIdCaloria.TabIndex = 2;
             // 
             // lblIdCaloria
             // 
@@ -82,22 +105,6 @@
             this.btnNuevo.Text = "Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = true;
             // 
-            // txtIdCaloria
-            // 
-            this.txtIdCaloria.Location = new System.Drawing.Point(111, 31);
-            this.txtIdCaloria.Name = "txtIdCaloria";
-            this.txtIdCaloria.Size = new System.Drawing.Size(159, 20);
-            this.txtIdCaloria.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 76);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Cantidad Calorias";
-            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.dtgListaCalorias);
@@ -114,13 +121,6 @@
             this.dtgListaCalorias.Name = "dtgListaCalorias";
             this.dtgListaCalorias.Size = new System.Drawing.Size(332, 147);
             this.dtgListaCalorias.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(111, 73);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(159, 20);
-            this.textBox1.TabIndex = 4;
             // 
             // lblTitulo
             // 
@@ -161,7 +161,7 @@
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnNuevo;
         private System.Windows.Forms.TextBox txtIdCaloria;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCantidad;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.DataGridView dtgListaCalorias;
